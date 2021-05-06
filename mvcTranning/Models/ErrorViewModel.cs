5d19b690e2274370eb4b9370e64503a61ffb0b28
jackwu55 <jackwu5x6@gmail.com>
2021-05-06 16:34:10 +0800

    test

---
+++ mvcTranning/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvcTranning.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
